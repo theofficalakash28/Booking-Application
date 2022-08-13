d8050b02fe47d987a9a6765755a8a222a2cf03f5
theofficialakash28 <theofficialakash28@gmail.com>
2022-08-13 21:03:23 +0530

    first commit~

---
+++ routes/users.js
@@ -0,0 +1,29 @@
+import express from "express";
+import { deleteUser, getUser, getUsers, updateUser } from "../controllers/user.js";
+import { verifyAdmin, verifyToken,verifyUser } from "../utils/verifyToken.js";
+
+const router = express.Router();
+
+//when ever we hit that url the verifytoken function will get called
+// router.get("/checkauthentication",verifyToken,(req,res,next) => {
+//     res.send("Hello user you are logged in!")
+// });
+
+// router.get("/checkuser/:id",verifyUser,(req,res,next)=>{
+//     res.send("hello, you are logged in and you can delete your account !")
+// });
+
+// router.get("/checkadmin/:id",verifyAdmin,(req,res,next)=>{
+//     res.send("hello, admin you are logged in and you can delete all account !")
+// });
+
+//UPDATE
+router.put("/:id",verifyUser, updateUser);
+//DELETE
+router.delete("/:id",verifyUser, deleteUser);
+//GET
+router.get("/:id",verifyUser,getUser);
+//GET ALL
+router.get("/", verifyAdmin, getUsers);
+
+export default router
